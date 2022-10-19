497281cc7de6ee96e51a0ea1c12e5acf5d187df4
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-10-19 20:25:50 +0400

    Init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
+import svgLoader from 'vite-svg-loader';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), svgLoader({ defaultImport: 'component' })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
