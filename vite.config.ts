03fba97b7182191db6a05245e723f8336a3ad1b1
Kirill Shevchenko <shevchenko.kirill192@gmail.com>
2022-10-19 21:50:31 +0400

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import { defineConfig } from 'vite';
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: '/opendota/',
-  plugins: [vue(), svgLoader({ defaultImport: 'component' })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  console.log(mode);
+  return {
+    base: '/opendota/',
+    plugins: [vue(), svgLoader({ defaultImport: 'component' })],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-}));
+  };
+});
